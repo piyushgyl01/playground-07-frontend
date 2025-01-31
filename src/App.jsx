a7eb351bd32a135bdb3f498acb0ff493e2b93e2b
piyush.gyl <piyush2022ug@gmail.com>
2025-01-31 13:14:15 +0530

    fix: centered hero image

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ export default function App() {
     <>
       <main className="container my-5">
         <section className="my-5">
-          <img
-            src="https://tse3.mm.bing.net/th?id=OIG1.QU2ug31_Eptcm9MVrtFJ&pid=ImgGn"
-            className="img-fluid rounded"
-            alt="hero-img"
-          />
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8">
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIG1.QU2ug31_Eptcm9MVrtFJ&pid=ImgGn"
+                className="img-fluid rounded"
+                alt="hero-img"
+              />
+            </div>
+          </div>
+          <div className="col-md-2"></div>
         </section>
         <section className="my-5 text-center">
           <h1>Todos</h1>
           <p>Welcome to the Todo application!</p>
-          <Link className="btn btn-primary" to={"/todos"}>View Todos</Link>
+          <Link className="btn btn-primary" to={"/todos"}>
+            View Todos
+          </Link>
         </section>
       </main>
     </>
